4767a64994e00bcbc20bcb47621f043d54ecbe38
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-05-12 19:02:01 +0530

    style - app view

---
+++ src/components/AddTransaction.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const AddTransaction = () => {
+	return (
+		<div className="form-wrapper">
+			<form>
+				<div className="input-group income">
+					<input type="text" placeholder="add income" autoComplete="off" />
+					<input type="number" placeholder="amount" autoComplete="off" />
+					<input type="submit" value="Submit" />
+				</div>
+			</form>
+			<form>
+				<div className="input-group expense">
+					<input
+						type="text"
+						placeholder="add expense"
+						autoComplete="off"
+					/>
+					<input type="number" placeholder="amount" autoComplete="off" />
+					<input type="submit" value="Submit" />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default AddTransaction;
