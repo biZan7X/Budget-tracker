af568f8c9141094949fabc504d95c91b941ec478
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-05-13 07:16:47 +0530

    feat - displaying the calculated balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
+	const { incomeTransactions, expenseTransactions } =
+		useContext(GlobalContext);
+
+	let totalIncome = 0;
+	let totalExpense = 0;
+
+	incomeTransactions.forEach(
+		(incomeTransaction) => (totalIncome += incomeTransaction.incomeAmount)
+	);
+
+	expenseTransactions.forEach(
+		(expenseTransaction) => (totalExpense += expenseTransaction.expenseAmount)
+	);
+
 	return (
 		<div className="balance">
 			<h2>Your balance</h2>
-			<h3>₹500</h3>
+			<h3>₹{(totalIncome - totalExpense).toFixed(2)}</h3>
 			<div className="income-expense">
 				<div className="plus">
 					<h3>income</h3>
-					<p>+₹0.00</p>
+					<p>+₹{totalIncome.toFixed(2)}</p>
 				</div>
 				<div className="minus">
 					<h3>expense</h3>
-					<p>-₹0.00</p>
+					<p>-₹{totalExpense.toFixed(2)}</p>
 				</div>
 			</div>
 		</div>
